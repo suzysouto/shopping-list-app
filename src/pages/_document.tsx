c7a366df0cf4c714c7fea088e7c8a0c21e70e499
suzysouto <suzysoutosilva@gmail.com>
2025-05-05 09:58:19 -0300

    tring to fix login with google issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>        
+        <meta name="referrer" content="no-referrer-when-downgrade" />
+      </Head>
       <body>
         <Main />
         <NextScript />
